29e50761e7968dc6459cf1bfaa5070c5be3e5200
Matt Deuschle <mattdeuschle@Matts-MacBook-Air.local>
2020-12-06 18:31:42 -0800

    update hello world

---
+++ LABiz/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Matt Deuschle on 12/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LABiz/Shared/LABizApp.swift
@@ -0,0 +1,17 @@
+//
+//  LABizApp.swift
+//  Shared
+//
+//  Created by Matt Deuschle on 12/6/20.
+//
+
+import SwiftUI
+
+@main
+struct LABizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
